779b70d9982459551c6fc8271df2d64d7bc5addd
amirsahra <amirhosein.sahra@gmail.com>
2023-01-15 10:25:10 +0330

    feat:create province api controller

---
+++ app/Http/Controllers/V1/IranProvinceAPiController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class IranProvinceAPiController extends Controller
+{
+    public function get()
+    {
+        $iranLocationsApiEndpoint = "https://iran-locations-api.vercel.app/api/v1/states";
+
+        $provincesPayload  = Http::get($iranLocationsApiEndpoint);
+        $provincesPayloadBody = $provincesPayload->body();
+
+
+        //return ($provincesPayload->body());
+    }
+}
